045cd38e345983991f8b0291945add855beb40c8
Alison Leme <alisonletos@hotmail.com>
2022-05-30 22:40:57 -0300

    Layout da página de filme

--- pages/user/[category]/[movieName]/[id].js
+++ pages/user/[category]/[movieName]/[id].js
@@ -1,21 +1,115 @@
+import {
+  Grid,
+  Box,
+  Typography,
+  Chip,
+  Card,
+  CardHeader,
+  CardMedia,
+  Avatar,
+} from "@mui/material";
+
 import TemplateDefault from "../../../../src/templates/Default";
 import dbConnect from "../../../../src/utils/dbConnect";
 import MoviesModel from "../../../../src/models/movies";
 
+import styles from "./movie.module.css";
+
 const Movie = ({ movie }) => {
+  console.log(movie);
   const url = movie.url;
   const urlCorreta = url.replace("watch?v=", "embed/");
 
   return (
     <TemplateDefault>
-      <iframe
-        width="560"
-        height="315"
-        src={urlCorreta}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-      ></iframe>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        className={styles.title}
+      >
+        {` Filme de ${movie.genero} `}
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={9}>
+          <Box className={styles.box}>
+            <Typography
+              component={"h4"}
+              variant={"h4"}
+              color="secondary.light"
+              align="center"
+              sx={{ mb: 2 }}
+            >
+              {movie.movieName}
+            </Typography>
+            <iframe
+              width="100%"
+              height={"500px"}
+              src={urlCorreta}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            ></iframe>
+          </Box>
+          <Box className={styles.box}>
+            <Typography
+              component={"span"}
+              variant={"caption"}
+              className={styles.ano}
+              color="secondary.light"
+            >
+              {`Ano Lançamento: ${movie.anoLancamento}`}
+            </Typography>
+            <Typography
+              component={"h6"}
+              variant={"h6"}
+              color="secondary.light"
+              className={styles.titleDescription}
+            >
+              Descrição
+            </Typography>
+            <Typography
+              component={"p"}
+              variant={"body2"}
+              className={styles.description}
+              color="secondary.light"
+            >
+              {movie.description}
+            </Typography>
+            <Chip
+              label={movie.genero}
+              className={styles.chip}
+              color="secondary"
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} lg={3}>
+          <Box className={styles.box}>
+            <Typography
+              component={"h4"}
+              variant={"h4"}
+              color="secondary.light"
+              align="center"
+              sx={{ mb: 2 }}
+            >
+              Publicado por:
+            </Typography>
+            <Card className={styles.card}>
+              <CardHeader
+                avatar={<Avatar>{movie.user.name[0]}</Avatar>}
+                title={movie.user.name}
+                subheader={movie.user.email}
+                className={styles.cardHeader}
+              />
+              <CardMedia
+                image="A"
+                title="Alison"
+                className={styles.cardMedia}
+              />
+            </Card>
+          </Box>
+        </Grid>
+      </Grid>
     </TemplateDefault>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,23 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from '@mui/material/colors';
+import { red } from "@mui/material/colors";
+import { colors } from "@mui/material";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#1F0626'
-        },
-        secondary: {
-            main: '#c5b1d6'
-        },
-        error: {
-            main: red.A400,
-        },
-    }
+  palette: {
+    background: {
+      default: "#e5e5e5",
+    },
+    primary: {
+      main: "#1F0626",
+    },
+    secondary: {
+      main: "#c5b1d6",
+      light: "#ffff",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
 });
 
 export default theme;
